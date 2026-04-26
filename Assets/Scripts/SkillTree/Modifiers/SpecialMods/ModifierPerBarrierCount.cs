6617b83d459b962bf9c97a1b2518af513397eb72
Ilia <i.olokin@mail.ru>
2026-04-26 11:45:36 +0200

    new nodes

---
+++ Assets/Scripts/SkillTree/Modifiers/SpecialMods/ModifierPerBarrierCount.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Battle;
+using LocalizationSupport;
+using UnityEngine;
+
+namespace SkillTree
+{
+    [CreateAssetMenu(menuName = "Modifiers/Special/Modifier Container Per Barrier Count", fileName = "New ModifierContainerPerBarrierCount")]
+    public class ModifierPerBarrierCount : Modifier
+    {
+        [SerializeField] private ModifierContainer modifierContainer;
+
+        public override void ApplyEffect(Unit unit)
+        {
+            if (modifierContainer == null)
+            {
+                return;
+            }
+
+            int barrierCount = (int)StatCalculator.GetStat(unit.BaseUnitModifiers, StatType.BarrierCount);
+            if (barrierCount <= 0)
+            {
+                return;
+            }
+
+            unit.BaseUnitModifiers.ChangeModifierValue(modifierContainer * barrierCount);
+        }
+
+        public override string GetDescription()
+        {
+            return GameLocalization.FormatModifier(
+                "modifier.modifierPerBarrierCount.description",
+                "Adds '[[0]]' per Barrier Count",
+                modifierContainer.GetDescription());
+        }
+    }
+}
